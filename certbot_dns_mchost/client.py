d56cef4a70e71c2b3852a3fa98c33f818671d568
Alekseenkov Daniil <ljdswer@gmail.com>
2026-01-02 18:28:11 +0300

    Implement record deletion

--- certbot_dns_mchost/client.py
+++ certbot_dns_mchost/client.py
@@ -20,6 +20,8 @@ MCHOST_LOGIN_URL = "https://my.mchost.ru/j_spring_security_check?ajax=true"
 MCHOST_ORDERS_URL = "https://my.mchost.ru"
 MCHOST_ZONE_FROM_ORDER_URL = "https://my.mchost.ru/dnsOrder/administration"
 MCHOST_CREATE_RECORD_URL = "https://my.mchost.ru/dnsZone/createRecord"
+MCHOST_ZONE_RECORDS_URL = "https://my.mchost.ru/dnsZone/records"
+MCHOST_DELETE_RECORD_URL = "https://my.mchost.ru/dnsZone/deleteRecord"
 
 
 class McHostClient:
@@ -48,12 +50,25 @@ class McHostClient:
             headers=POST_HEADERS,
         )
         if response.status_code != 200:
-            logger.error(f"Server returned an error: {response.content}")
+            logger.error(
+                f"Server returned an error while creating a record: {response.content}"
+            )
             raise PluginError("Unable to create a record")
 
     def del_txt_record(self, domain_name: str, record_name: str, record_content: str):
-        # TODO:
-        pass
+        (domain_base, domain_id) = self.get_id_for_domain(domain_name)
+        record_name = record_name[: -(len(domain_base) + 1)]
+        record = self.get_txt_records(domain_id).get((record_name, record_content))
+        if not record:
+            logger.error(f"Tried to delete non-existing TXT record: {record_name}")
+        else:
+            response = self._session.post(
+                MCHOST_DELETE_RECORD_URL,
+                params={"id": domain_id, "recordId": record},
+                headers=POST_HEADERS,
+            )
+            if response.status_code != 200:
+                raise PluginError("Unable to delete a record")
 
     @staticmethod
     def login(user: str, passwd: str) -> Session:
@@ -115,3 +130,19 @@ class McHostClient:
                 if "dnsOrder" in href:
                     result[str(name)] = str(href).split("/")[-1]
         return result
+
+    def get_txt_records(self, zone_id: int) -> dict[tuple[str, str], int]:
+        response = self._session.post(
+            MCHOST_ZONE_RECORDS_URL, params={"id": zone_id}, headers=POST_HEADERS
+        )
+        data = response.json().get("data")
+        if not data:
+            raise PluginError("No content retured from server for the zone")
+        records = data.get("records")
+        if not records:
+            raise PluginError("No content retured for records")
+        return {
+            (r["name"], r["content"]): r["id"]
+            for r in records
+            if "id" in r and r["type"] == "TXT"
+        }
